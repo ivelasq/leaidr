789e8eee1f64754908d36016f854dfa95f9df483
Isabella Velásquez <ivelasq@gmail.com>
2020-04-30 22:02:19 -0500

    trying this out...

--- R/create_shapefile.R
+++ R/create_shapefile.R
@@ -11,9 +11,9 @@
 
 create_shapefile <- function(state_abb){
   message("This may take a while...")
-  dis_map_rds <- leaidr::schooldistrict_rda
-  writeOGR(dis_map_rds, dsn = "./shapefile", layer = "schooldistrict_sy1819_tl19", driver = "ESRI Shapefile")
-  dis_map <- rgdal::readOGR("./shapefile", "schooldistrict_sy1819_tl19")
+  pb_download()
+  writeOGR(schooldistrict_rda, dsn = "./default/Downloads", layer = "schooldistrict_sy1819_tl19", driver = "ESRI Shapefile")
+  dis_map <- rgdal::readOGR("./default/Downloads", "schooldistrict_sy1819_tl19")
   if(state_abb == "All"){
     dis_map
   } else {
