65aa53ee447d7470f8db59d8b8eaa0e33914f8cd
Isabella Velásquez <ivelasq@gmail.com>
2020-04-30 20:06:38 -0500

    remove old files; update shapefile

--- R/create_shapefile.R
+++ R/create_shapefile.R
@@ -11,7 +11,7 @@
 
 create_shapefile <- function(state_abb){
   message("This may take a while...")
-  dis_map_rds <- leaidr::dis_map
+  dis_map_rds <- leaidr::schooldistrict_rda
   writeOGR(dis_map_rds, dsn = "./shapefile", layer = "schooldistrict_sy1819_tl19", driver = "ESRI Shapefile")
   dis_map <- rgdal::readOGR("./shapefile", "schooldistrict_sy1819_tl19")
   if(state_abb == "All"){
